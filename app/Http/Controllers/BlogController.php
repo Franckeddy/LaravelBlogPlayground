9e4fd650b03746c099512541bf042286af423718
Franck Garcon <franck.garcon@gmail.com>
2023-07-02 22:42:52 +0200

    Add Controller's Docs

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,22 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\BlogFilterRequest;
 use App\Models\Post;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
 use Illuminate\Validation\Rule;
 
 class BlogController extends Controller
 {
+    /**
+     * @return View
+     */
     public function index(): View {
         return view('blog.index', [
             'posts' => Post::paginate(1)
         ]);
     }
 
+    /**
+     * @param string $slug
+     * @param Post $post
+     * @return RedirectResponse|View
+     */
     public function show(string $slug, Post $post): RedirectResponse | View {
         if ($post->slug !== $slug) {
             return to_route('blog.show', [
--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    public function index()
+    /**
+     * @return View
+     */
+    public function index(): View
     {
         return view('welcome');
     }
